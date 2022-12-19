1a74037c3ca44bae245bccf7dacea3ee1a448ad0
davetrux <david.truxall@graphitehealth.io>
2022-12-19 17:49:46 -0500

    Next two

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+  let largestResult = 0
+  for(let i = 0; i < inputArray.length-1; i++) {
+    const temp = inputArray[i] * inputArray[i+1]
+    if (temp > largestResult)
+      largestResult = temp
+  }
+  return largestResult
 }
 
 //console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,6 @@
 export function allLongestStrings(inputArray: string[]): string[] {
-
+  const longest = Math.max(...inputArray.map(el => el.length))
+  return inputArray.filter(item => item.length === longest )
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
